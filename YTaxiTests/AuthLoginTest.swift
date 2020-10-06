c5afdd6e868a411a910f809d96b199039c55eea3
abdul diallo <rahimi.diallo96@gmail.com>
2020-10-06 11:33:47 -0400

    test login service

--- YTaxiTests/AuthLoginTest.swift
+++ YTaxiTests/AuthLoginTest.swift
@@ -13,10 +13,46 @@ import Quick
 class AuthLoginTest : QuickSpec {
     
     override func spec() {
-        describe("login to firebase") {
-            context("when a user enter credentials") {
-                it("should return response from the server") {
-                    <#code#>
+        describe("To firebase")  {
+            var loginService : LoginServiceMock!
+            let email = "johndoe@gmail.com"
+            let password = "123456"
+            beforeEach {
+                loginService = LoginServiceMock(false)
+            }
+            afterEach {
+                loginService = nil
+            }
+            
+            describe("when the server is running properly") {
+                context("given the loggin & logout function") {
+                    it("should not return any error") {
+                        loginService.login(withEmail: email, and: password) { error in
+                            expect(error).to(beNil())
+                        }
+                    }
+                    it("should not return any error") {
+                        loginService.logout { error in
+                            expect(error).to(beNil())
+                        }
+                    }
+                }
+            }
+            
+            describe("when server is not running") {
+                context("given the loggin & logout function") {
+                    it("should return loggin error") {
+                        loginService.shouldReturnError = true
+                        loginService.login(withEmail: email, and: password) { error in
+                            expect(error?.localizedDescription).toNot(beNil())
+                        }
+                    }
+                    it("should return logout error") {
+                        loginService.shouldReturnError = true
+                        loginService.logout { error in
+                            expect(error?.localizedDescription).toNot(beNil())
+                        }
+                    }
                 }
             }
         }
